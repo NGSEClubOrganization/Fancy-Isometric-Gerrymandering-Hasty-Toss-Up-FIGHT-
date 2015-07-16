cf7028cd115eb8c2b539e92f0f7226920cf0b3a0
Riib <henry@monkeyface.com>
2015-07-15 21:05:50 -0700

    I added a new system for abilites, now it is much simpler:
    Instead of there being a lot of things that are inherent to classes that
    extend Ability (all in the fight.specials package), now you just have ot
    have the effects along with the name and MID (magic id)
    
    The MID is so that when a player click,s the listener checks to see if
    the lore of the item in hand matches the MID's of any of the abilities
    that player's class has. If it does, that specific ability is activated.
    THE LORE IS THE PART OF THE ITEM THAT IS CHECKED. I tried to find a way
    to attach the <Ability> directly, but ItemMeta only holds Strings in the
    form of Display Name and Lore, so I used lore for the operational part.

--- TestPlugin-2/TestPlugin-2/src/com/ngse/fight/classes/Ability.java
+++ TestPlugin-2/TestPlugin-2/src/com/ngse/fight/classes/Ability.java
@@ -1,48 +1,59 @@
 package com.ngse.fight.classes;
 
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.potion.PotionEffect;
+import java.util.ArrayList;
 
-import com.ngse.fight.FightPlayer;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 public abstract class Ability {
 
-	public FightPlayer user;
+	public int level;
+	public boolean isPassive;
+	public ItemStack item;
+	public String name;
+	public String MID;
 
-	public boolean passive;
-	public ItemStack itemsRecieved;
+	// all abilities are attched to items
 
-	public Ability(FightPlayer user, boolean passive, ItemStack itemsRecieved) {
-		super();
-		this.user = user;
-		this.passive = passive;
-		this.itemsRecieved = itemsRecieved;
+	public Ability(String name, int level, boolean isPassive, String MID) {
+		this.name = name;
+		this.level = level;
+		this.isPassive = isPassive;
+		this.item = getItem();
+		this.MID = MID;
 	}
 
-	public FightPlayer getUser() {
-		return user;
+	public abstract void effect(Player user, Player target);
+
+	public abstract void effect(Player user);
+
+	public String getName() {
+		return name;
 	}
 
-	public void setUser(FightPlayer user) {
-		this.user = user;
+	public int getLevel() {
+		return level;
 	}
 
 	public boolean isPassive() {
-		return passive;
+		return isPassive;
 	}
 
-	public void setPassive(boolean passive) {
-		this.passive = passive;
+	public abstract ItemStack getItem();
+
+	public String getMID() {
+		return MID;
 	}
 
-	public ItemStack getItemsRecieved() {
-		return itemsRecieved;
+	public static ItemStack setupItem(Material m, Ability a) {
+		ItemStack i = new ItemStack(m, 1);
+		i.setType(m);
+		i.getItemMeta().setDisplayName(a.getName());
+		ArrayList<String> l = new ArrayList<String>();
+		l.add(a.getMID());
+		i.getItemMeta().setLore(l);
+		return i;
 	}
-
-	public void setItemsRecieved(ItemStack itemsRecieved) {
-		this.itemsRecieved = itemsRecieved;
-	}
-
-	public abstract void specificEffect();
 
 }
